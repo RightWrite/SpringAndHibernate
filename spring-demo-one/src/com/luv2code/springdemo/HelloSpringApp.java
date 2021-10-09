3339d3a93bc403a6c3fdc23d38bb4cc5b2f56795
RightWrite <21136428+RightWrite@users.noreply.github.com>
2021-10-09 21:38:24 +0100

    Inversion of Control - XML configuration

---
+++ spring-demo-one/src/com/luv2code/springdemo/HelloSpringApp.java
@@ -0,0 +1,20 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+
+  public static void main(String[] args) {
+    // Load spring config file
+    ClassPathXmlApplicationContext context= new ClassPathXmlApplicationContext("applicationContext.xml");
+
+    // Retrieve bean from container
+    Coach coach=context.getBean("theCoach",Coach.class);
+    // call method on the bean
+    System.out.println(coach.getDailyWorkout());
+    // Close application context
+    context.close();
+
+  }
+}
